e3a6c584f26bd6786114b36ffdd7e325cd016b67
Adnane2030 <git config --global user.email>
2022-11-04 22:12:27 +0000

    1.8

--- mail.php
+++ mail.php
@@ -16,6 +16,15 @@ echo'
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CLW82VST4C"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CLW82VST4C');
+</script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
